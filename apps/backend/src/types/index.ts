98bba03919faf6765dd086adaadaf83c6be824a9
Prashant Kumar <prashantkumar1251@gmail.com>
2025-04-19 01:38:18 +0530

    bug fix

--- apps/backend/src/types/index.ts
+++ apps/backend/src/types/index.ts
@@ -1,48 +1,30 @@
 import z from "zod";
 
-<<<<<<< HEAD
 export type expiresInType = `${number}${'M' | 'Y' | 'D'}`;
-=======
-export type expiresInType = `${number}${"M" | "Y" | "D"}`;
->>>>>>> ddf9311f28e98659bd37cf2db8a0e551f6ee67fd
 
 export const signupSchema = z.object({
   email: z.string().email(),
   name: z.string(),
   password: z.string().min(8),
   role: z.enum(["user", "admin"]),
-<<<<<<< HEAD
 })
-=======
-});
->>>>>>> ddf9311f28e98659bd37cf2db8a0e551f6ee67fd
 
 export const signinSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
-<<<<<<< HEAD
 })
 
 export const publicKeySchema = z.object({
   publicKey: z.string()
-=======
 });
-
-export const publicKeySchema = z.object({
-
-    publicKey: z.string()
->>>>>>> ddf9311f28e98659bd37cf2db8a0e551f6ee67fd
-})
 
 export const squadSchema = z.object({
   name: z.string(),
   logo: z.string(),
   captain: z.string(),
   viceCaptain: z.string(),
-
 })
 
-<<<<<<< HEAD
 export const playerSchema = z.object({
   name: z.string(),
   role: z.enum(["batsman", "bowler", "wk", "ar"]),
@@ -56,14 +38,4 @@ export const playerUpdateSchema = z.object({
   playingStatus: z.enum(["playing", "notPlaying", "substitute"]),
   playerId: z.string(),
 })
-=======
-const playerSchema = z.object({
-    name: z.string(),
-    role: z.enum(["batsman", "bowler", "wk", "ar"]),
-    dexture: z.enum(["right", "left"]),
-    playingStatus: z.enum(["playing", "notPlaying", "substitute"]),
-    squadId: z.string(),
-    countryStatus: z.enum(["indian", "foreign"])
-})
 
->>>>>>> ddf9311f28e98659bd37cf2db8a0e551f6ee67fd
