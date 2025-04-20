6bb1ecd52ed697f885a7c39281eb117b8c59a2d9
Hamzah Khan <66473175+hamzahshahbazkhan@users.noreply.github.com>
2025-04-20 13:05:27 +0530

    updated schema

--- apps/backend/src/routes/v1/userTeams.ts
+++ apps/backend/src/routes/v1/userTeams.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
+import { userTeamsSchema } from "../../types/index";
 import { userMiddleware } from "../../middleware/user";
 
 export const userTeamsRouter = Router();
 userTeamsRouter.use(userMiddleware);
 
+userTeamRouter.post("/create", async (req: any, res: any) => {
+  const parsedData = userTeamsSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    return res.status(400).json({ message: "data validation failed" });
+  }
+});
